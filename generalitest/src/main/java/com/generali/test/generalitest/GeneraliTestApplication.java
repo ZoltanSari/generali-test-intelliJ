c8cbb9ad2c02b8711a38f4067c0f54546162b31a
ZoltanSari <Kingshapy@gmail.com>
2019-02-13 09:37:11 +0100

    add gitignore

---
+++ generalitest/src/main/java/com/generali/test/generalitest/GeneraliTestApplication.java
@@ -0,0 +1,13 @@
+package com.generali.test.generalitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneraliTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneraliTestApplication.class, args);
+    }
+}
+
